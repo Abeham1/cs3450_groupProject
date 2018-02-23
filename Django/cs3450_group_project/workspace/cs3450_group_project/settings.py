5b8b20ab443c0b4e05582787262a49a1e315b81a
Abe Gunther <abraham.gunther@hotmail.com>
2018-02-23 17:05:54 +0000

    I started our virtual_waitress app in django and began inserting URL views

--- Django/cs3450_group_project/workspace/cs3450_group_project/settings.py
+++ Django/cs3450_group_project/workspace/cs3450_group_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'virtual_waitress',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ Django/cs3450_group_project/workspace/virtual_waitress/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VirtualWaitressConfig(AppConfig):
+    name = 'virtual_waitress'
