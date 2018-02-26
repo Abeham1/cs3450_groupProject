748f37d0d41fc0defd289c6da6c412c136392a76
Aaron Fine <fine.aaron@gmail.com>
2018-02-26 16:37:57 -0700

    Add Inventory Screen to Django Admin Panel
    Added bones of inventory models

--- Django/cs3450_group_project/workspace/virtual_waitress/models.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class AllowedItems(models.Model):
+    name = models.CharField("Item Name", max_length=250)
+    units = models.CharField(max_length=50)
+
+class Items(models.Model):
+    name = models.ForeignKey(AllowedItems, on_delete=models.CASCASE)
+    loc = models.CharField("Item Location", max_length=250)
+    qty = models.PositiveIntegerField(default=0, localize=False)
+    cost = models.FloatField("Unit Cost", default=0, localize=False)
+    expiration = models.DateField("Expiration Date")
+    vendor = models.CharField(max_length=250)
