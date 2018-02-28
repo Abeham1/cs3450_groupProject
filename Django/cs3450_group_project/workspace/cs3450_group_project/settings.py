57b65aec88f1e9006bd728a2527d5892bacf063e
Aaron Fine <fine.aaron@gmail.com>
2018-02-27 22:10:23 -0700

    Add Inventory Screen to Django Admin Panel
    Removed MiddleWare that no longer works with Django 2.0 from Settings File
    Added timezone to Settings File
    Fixed typos in models
    Added better names to models
    Update gitignore file for Visual Studio Code

--- Django/cs3450_group_project/workspace/cs3450_group_project/settings.py
+++ Django/cs3450_group_project/workspace/cs3450_group_project/settings.py
@@ -38,16 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'virtual_waitress',
+    'virtual_waitress.apps.VirtualWaitressConfig',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -108,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
--- Django/cs3450_group_project/workspace/virtual_waitress/models.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/models.py
@@ -6,11 +6,23 @@ from django.db import models
 class AllowedItems(models.Model):
     name = models.CharField("Item Name", max_length=250)
     units = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Allowable Inventory Item"
 
 class Items(models.Model):
-    name = models.ForeignKey(AllowedItems, on_delete=models.CASCASE)
+    name = models.ForeignKey(AllowedItems, on_delete=models.CASCADE)
     loc = models.CharField("Item Location", max_length=250)
-    qty = models.PositiveIntegerField(default=0, localize=False)
-    cost = models.FloatField("Unit Cost", default=0, localize=False)
+    qty = models.PositiveIntegerField(default=0)
+    cost = models.FloatField("Unit Cost", default=0)
     expiration = models.DateField("Expiration Date")
     vendor = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Inventory Item"
+        
