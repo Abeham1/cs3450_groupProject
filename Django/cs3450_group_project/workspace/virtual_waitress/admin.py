7b52d48774020a9151b16c775f257d0d8412017d
Abeham1 <abraham.gunther@hotmail.com>
2018-03-17 15:41:15 -0600

    Made Database object data transfer to Template HTML

--- Django/cs3450_group_project/workspace/virtual_waitress/admin.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import AllowedItems, Items
+from .models import AllowedItems, Items, RestaurantName
 
 # Register your models here.
+admin.site.register(RestaurantName)
 admin.site.register(AllowedItems)
 admin.site.register(Items)
--- Django/cs3450_group_project/workspace/virtual_waitress/views.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+from virtual_waitress.models import RestaurantName
 
 def index (request):
     context = {
@@ -24,10 +26,22 @@ def review (request):
     }
     return render(request, 'virtual_waitress/customer_review.html', context)
 
-def inventory (request):
+def inventory (request): #To send model data from Database to Javascript/Template
+    # For multiple, or complex data types, 
+    # you have a queryset. This must be translated into JSON data
+    # 1. Save the queryset to a variable.
+    result = RestaurantName.objects.all() 
+    #2. call the .values() function on your variable to get data contents. 
+    #  Use list() to to put it into serializable format
+    lst = (list(result.values())) #
     context = {
         'activePage':'inventory',
-        'resturantName': 'Welcome To Test!'
+        #3. JSON.dumps converts previous variable to JSON
+        # note: when bringing variable into javascript, escapejs filter 
+        # must be used in tempalte
+        'resturantName': json.dumps(lst),
+        # For single dataobjects, just create variable and pass in by Primary Key
+        'foo': RestaurantName.objects.get(pk=1)
     }
     return render(request, 'virtual_waitress/inventory.html', context)
 
