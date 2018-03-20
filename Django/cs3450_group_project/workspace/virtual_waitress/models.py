70aee877332ee9697a3cfe8839636b77e38fc881
Aaron Fine <fine.aaron@gmail.com>
2018-03-20 09:09:06 -0600

    Fixed issues with base template and duplicate menu views

--- Django/cs3450_group_project/workspace/virtual_waitress/models.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/models.py
@@ -1,19 +1,23 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import CASCADE
+
 
 # Create your models here.
 class AllowedItems(models.Model):
     name = models.CharField("Item Name", max_length=250)
     units = models.CharField(max_length=50)
+
     def __str__(self):
-        return self.name
-    
+        return str(self.name)
+
     class Meta:
         verbose_name = "Allowable Inventory Item"
 
+
 class Items(models.Model):
-    name = models.ForeignKey(AllowedItems, on_delete=models.CASCADE)
+    itemType = models.ForeignKey(AllowedItems, on_delete=CASCADE)
     loc = models.CharField("Item Location", max_length=250)
     qty = models.PositiveIntegerField(default=0)
     cost = models.FloatField("Unit Cost", default=0)
@@ -21,17 +25,17 @@ class Items(models.Model):
     vendor = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.name
+        return str(self.itemType)
 
     class Meta:
         verbose_name = "Inventory Item"
-        
+
 
 class RestaurantName(models.Model):
     name = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
         verbose_name = "Restaurant Name"
--- Django/cs3450_group_project/workspace/virtual_waitress/views.py
+++ Django/cs3450_group_project/workspace/virtual_waitress/views.py
@@ -2,52 +2,56 @@ from django.shortcuts import render
 import json
 from virtual_waitress.models import RestaurantName
 
-def index (request):
-    context = {
-        'activePage':'menu'
-    }
-    return render(request, 'virtual_waitress/menu.html', context)
 
-def manager (request):
+def manager(request):
     context = {
-        'activePage':'manager'
+        'activePage': 'manager'
     }
     return render(request, 'virtual_waitress/manager.html', context)
 
-def cook (request):
+
+def cook(request):
+    result = RestaurantName.objects.all()
+    lst = (list(result.values()))
     context = {
-        'activePage':'cook'
+        'activePage': 'cook',
+        'restaurantName': json.dumps(lst),
     }
     return render(request, 'virtual_waitress/cook_view.html', context)
 
-def review (request):
+
+def review(request):
     context = {
-        'activePage':'review'
+        'activePage': 'review'
     }
     return render(request, 'virtual_waitress/customer_review.html', context)
 
-def inventory (request): #To send model data from Database to Javascript/Template
+
+def inventory(request):  # To send model data from Database to Javascript/Template
     # For multiple, or complex data types, 
     # you have a queryset. This must be translated into JSON data
     # 1. Save the queryset to a variable.
-    result = RestaurantName.objects.all() 
-    #2. call the .values() function on your variable to get data contents. 
+    result = RestaurantName.objects.all()
+    # 2. call the .values() function on your variable to get data contents.
     #  Use list() to to put it into serializable format
-    lst = (list(result.values())) #
+    lst = (list(result.values()))  #
     context = {
-        'activePage':'inventory',
-        #3. JSON.dumps converts previous variable to JSON
+        'activePage': 'inventory',
+        # 3. JSON.dumps converts previous variable to JSON
         # note: when bringing variable into javascript, escapejs filter 
         # must be used in tempalte
-        'resturantName': json.dumps(lst),
+        'restaurantName': json.dumps(lst),
         # For single dataobjects, just create variable and pass in by Primary Key
-        'foo': RestaurantName.objects.get(pk=1)
+        'foo': RestaurantName.objects.get(pk=1),
     }
     return render(request, 'virtual_waitress/inventory.html', context)
 
-def menu (request):
+
+def menu(request):
+    result = RestaurantName.objects.all()
+    lst = (list(result.values()))
     context = {
-        'activePage':'menu',
-        'resturantName': 'Welcome To Test!'
+        'activePage': 'menu',
+        'restaurantName': json.dumps(lst),
     }
     return render(request, 'virtual_waitress/menu.html', context)
